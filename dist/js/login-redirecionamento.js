f6436600874f195adb886690d22e4650ac0e9418
squadBI <93922904+squadBI@users.noreply.github.com>
2025-05-06 14:33:57 -0300

    update sistema

--- dist/js/login-redirecionamento.js
+++ dist/js/login-redirecionamento.js
@@ -11,21 +11,16 @@ hash_gerada_randomicamente = generateRandomHash(32); // Gera uma hash de 32 cara
 check_email=false;
 email='';
 usuario='';
-tipo='';
-posicao='';
-acessos_segmento='';
+userId='';
 
 async function verifica_usuario(input_email){
-    var url = 'https://prod-11.westus.logic.azure.com:443/workflows/17d85237df584fa7b79044b8660db8f4/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=v_MLtOtoYtQGLlPUVOixr98MdrNAwNtV8nUE3dxxEAQ';
+    var url = 'https://prod-13.westus.logic.azure.com:443/workflows/33f2b697599f43bbb2feb880995d6afa/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_yc3eSZUh6sEGxZoKMQeNahrfG3SWjkOq_gWroyaE-M';
 
     var login_data = {
-        solicitacao: "oauth",
-        hash: hash_gerada_randomicamente,
-        email: input_email,
-        x_api_key: "1348f1ed93616d59f6d62eb7631ae137cc902fc9cc4bb22428a384c1cec91c20"
+        
     };
 
-    await fetch(url,{
+    fetch(url,{
         method: 'POST',
         headers: {
             "Content-Type": "application/json"
@@ -35,25 +30,22 @@ async function verifica_usuario(input_email){
         .then(response => response.json())
         .then(data => {
             dados = JSON.parse(data.result);
+            dados = dados.value;
             for(i=0; i<dados.length; i++){
                 if(dados[i].email==input_email){
                     check_email=true;
                     email=input_email;
-                    raiz_email=dados[i].raiz_email;
-                    usuario=dados[i].usuario;
-                    tipo=dados[i].tipo;
-                    posicao=dados[i].posicao;
-                    acessos_segmento=dados[i].acessos_segmento;
+                    userId=dados[i].userId;
+                    usuario=dados[i].displayName;
                 }
             }
             if(check_email){
-                $("#exibe_input_email").hide();
-                $("#exibe_codigo_verificacao").show();
-                $("#verifica_usuario").hide();
-                $("#verifica_hash").show();
-                console.log('o usuário existe proxima etapa de verificacao');
+                localStorage.setItem('userId', userId);
+                localStorage.setItem('email', email);
+                localStorage.setItem('usuario', usuario);
+                window.location.assign('./');
             } else {
-                console.log('o usuário não existe ou não tem permissão para acessar');
+                console.log('o usuário não tem permissão para acessar');
             }
         })
         .catch(error => {
@@ -61,28 +53,6 @@ async function verifica_usuario(input_email){
         })
 }
 
-function verifica_hash(input_hash){
-    if(input_hash==hash_gerada_randomicamente){
-        localStorage.setItem('usuario', usuario);
-        localStorage.setItem('email', email);
-        localStorage.setItem('raiz_email', raiz_email);
-        localStorage.setItem('tipo', tipo);
-        localStorage.setItem('posicao', posicao);
-        localStorage.setItem('acessos_segmento', acessos_segmento);
-        window.location.assign('./');
-    } else {
-        $("#exibe_input_email").show();
-        $("#exibe_codigo_verificacao").hide();
-        $("#verifica_usuario").show();
-        $("#verifica_hash").hide();
-        console.log('o usuário não tem permissão para acessar');
-    }
-}
-
 $("#verifica_usuario").click(function(){
     verifica_usuario($("#input_email").val());
-});
-
-$("#verifica_hash").click(function(){
-    verifica_hash($("#input_hash_email").val());
 });
--- dist/js/verifica-login.js
+++ dist/js/verifica-login.js
@@ -1,34 +1,24 @@
 (async function () {
-        if(localStorage.getItem('usuario') && localStorage.getItem('email') && localStorage.getItem('tipo')){
+        if(localStorage.getItem('userId') && localStorage.getItem('email') && localStorage.getItem('usuario')){
             // Para pegar o valor desse item
-            usuario = localStorage.getItem('usuario');
+            userId = localStorage.getItem('userId');
             email = localStorage.getItem('email');
-            raiz_email = localStorage.getItem('raiz_email');
-            tipo = localStorage.getItem('tipo');
-            posicao = localStorage.getItem('posicao');
-            acessos_segmento = localStorage.getItem('acessos_segmento');
+            usuario = localStorage.getItem('usuario');
 
             await $("#usuario").text(usuario.split(' ')[0]+' '+usuario.split(' ')[usuario.split(' ').length-1]);
-            await $("#posicao").text(posicao);
+            await $("#posicao").text('POSICAO');
 
-            await $("#avatar").css('background-image','url(./static/avatars/'+raiz_email+'.jfif)');
-
-            if(tipo=='Administrador'){
-                $("#verifica_analista_usuario").show();
-            }
+            await $("#avatar").css('background-image','url(./static/avatars/8_orgid_'+userId+'.jfif)');
 
         } else {
-                window.location.assign('./verificacao-usuario');
+                window.location.assign('./');
         }
 })();
 
 
 $("#logout").click(function(){
-    localStorage.removeItem('usuario');
+    localStorage.removeItem('userId');
     localStorage.removeItem('email');
-    localStorage.removeItem('raiz_email');
-    localStorage.removeItem('tipo');
-    localStorage.removeItem('posicao');
-    localStorage.removeItem('acessos_segmento');
+    localStorage.removeItem('usuario');
     window.location.assign('./verificacao-usuario'); 
 });
